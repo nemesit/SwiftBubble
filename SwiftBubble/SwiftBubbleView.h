e5f8cf1958c11ed6f0052984aebfd8d7ca7c4fed
nemesit <nemesit@myriadvisuals.com>
2015-05-30 14:58:43 +0200

    Initial Commit

---
+++ SwiftBubble/SwiftBubbleView.h
@@ -0,0 +1,13 @@
+//
+//  SwiftBubbleView.h
+//  SwiftBubble
+//
+//  Created by Felix Grabowski on 30/05/15.
+//  Copyright (c) 2015 Felix Grabowski. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface SwiftBubbleView : ScreenSaverView
+
+@end
